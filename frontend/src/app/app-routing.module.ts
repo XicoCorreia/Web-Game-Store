fb5b6614a40e09b264138bcdd9e14e12f4bc04b9
XicoCorreia <xicocorreia17@gmail.com>
2023-04-22 19:22:02 +0100

    Profile page prototype created

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ItemsComponent } from './items/items.component';
 import { ItemDetailComponent } from './item-detail/item-detail.component';
+import { ProfileComponent } from './profile/profile.component';
 
 const routes: Routes = [
   { path: 'browse', component: ItemsComponent },
   { path: 'detail/:id', component: ItemDetailComponent },
+  { path: 'profile', component: ProfileComponent },
 ];
 
 @NgModule({
