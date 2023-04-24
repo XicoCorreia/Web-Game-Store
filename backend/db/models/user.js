ed851f43e6e4f5fb1cbda799a0816483ab96053d
João Gil <fc52812@alunos.ciencias.ulisboa.pt>
2023-04-24 10:47:18 +0100

    Small fix in user model

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -4,7 +4,7 @@ const {Item} = require("./item");
 const UserSchema = new mongoose.Schema({
     id : {type: Number,required:true,unique:true},
     username : {type: String,required: true,unique:true,minlength:3},
-    password : {type: String,required: true,unique:true},
+    password : {type: String,required: true},
     image : {type:String,required:true,
         default:'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460__340.png'},
     following : {type:[String],required:true,default:[]},
