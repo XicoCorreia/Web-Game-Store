28f183bc7dc0bc12d5b5bbdf0e69af213a710be0
Isabel Baena <fc54713@alunos.fc.ul.pt>
2023-04-26 14:05:41 +0100

    mostrar mensagem erro login

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { User } from '../../user';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -9,19 +10,22 @@ import { User } from '../../user';
 })
 export class LoginComponent {
   currentUser: User = {} as User;
+  feedback = '';
 
-  constructor(private authService: AuthService) {}
+
+  constructor(private authService: AuthService, private router: Router) {}
 
   login(name: string, pass: string): void {
     if (name !== '' && pass !== '') {
       this.authService.login(name, pass).subscribe((user: User) => {
         if (user != null) {
           this.currentUser = user;
+          this.feedback = 'Login efetuado com sucesso!';
+          this.router.navigate(['/profile', this.currentUser.username]);
+          //TODO quando o login é correto, deve ser redirecionado para o dashboard e nao perfil
         }
       });
     }
-    //if (name != '' && pass != '') {
-    // this.currentUser=this.authService.login(name,pass);
-    //}
+    this.feedback = 'Login falhou! Tente novamente.';
   }
 }
