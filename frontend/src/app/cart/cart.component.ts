2fb319b7499f14e560b3800abfb48765e30daeea
Alexandre Fonseca <fc55955@alunos.fc.ul.pt>
2023-05-05 23:47:00 +0100

    Update styling, add item count and total.

--- frontend/src/app/cart/cart.component.ts
+++ frontend/src/app/cart/cart.component.ts
@@ -1,21 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ItemService } from '../item.service';
 import { UserService } from '../user.service';
-import { Item } from '../../item';
+import { LineItem } from '../../line-item';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css'],
+  animations: [
+    trigger('slideInOut', [
+      transition(':enter', [
+        style({ opacity: 0, height: '0' }),
+        animate('200ms', style({ height: '*' })),
+        animate('100ms', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [
+        style({ opacity: 1, height: '*' }),
+        animate('100ms', style({ opacity: 0 })),
+        animate('200ms', style({ height: '0' })),
+      ]),
+    ]),
+  ],
 })
 export class CartComponent implements OnInit {
-  items!: Item[];
+  @Input() lineItems!: Set<LineItem>;
 
-  private iconMap: { [key: string]: string } = {
-    game: 'sports_esports',
-    subscription: 'subscriptions',
-    dlc: 'download_for_offline',
+  private iconMap: { [key: string]: { [key: string]: string } } = {
+    game: {
+      icon: 'sports_esports',
+      label: 'Game',
+    },
+    subscription: {
+      icon: 'subscriptions',
+      label: 'Subscription',
+    },
+    dlc: {
+      icon: 'download_for_offline',
+      label: 'DLC',
+    },
   };
 
   constructor(
@@ -24,14 +48,40 @@ export class CartComponent implements OnInit {
     private userService: UserService
   ) {}
 
-  getIcon(itemType: string) {
-    return this.iconMap[itemType];
-  }
-
   ngOnInit(): void {
     // TODO: store cart items in sessionStorage
     this.itemService.getItems().subscribe((items) => {
-      this.items = items.filter((item) => item.price > 0).slice(0, 6);
+      this.lineItems = new Set(
+        items
+          .filter((item) => item.price > 0)
+          .map((item) => {
+            return { item, count: ~~(Math.random() * 5) + 1 };
+          })
+          .slice(0, 6)
+      );
     });
   }
+
+  getIcon(itemType: string) {
+    return this.iconMap[itemType]['icon'];
+  }
+
+  getLabel(itemType: string) {
+    return this.iconMap[itemType]['label'];
+  }
+
+  add(li: LineItem) {
+    li.count++;
+  }
+
+  remove(li: LineItem) {
+    if (li.count > 1) {
+      li.count--;
+    } else {
+      this.lineItems.delete(li);
+    }
+  }
+
+  sum = () =>
+    [...this.lineItems].reduce((acc, li) => acc + li.count * li.item.price, 0);
 }
---
+++ frontend/src/line-item.ts
@@ -0,0 +1,6 @@
+import { Item } from './item';
+
+export type LineItem = {
+  item: Item;
+  count: number;
+};
